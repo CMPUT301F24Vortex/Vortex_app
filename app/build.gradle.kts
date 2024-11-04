0b183d3e34d332cc665ee300388c672697fbc9fc
Gyurim Do <gyurim@ualberta.ca>
2024-11-04 01:23:09 -0700

    I added my task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,7 +38,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+
+
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1")) // Firebase BOM for version management
+    implementation("com.google.firebase:firebase-firestore") // Firestore dependency without version
 }
---
+++ app/src/main/java/com/example/vortex_app/Center.java
@@ -0,0 +1,19 @@
+package com.example.vortex_app;
+
+public class Center {
+    private String name;
+    private String address;
+
+    public Center(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
