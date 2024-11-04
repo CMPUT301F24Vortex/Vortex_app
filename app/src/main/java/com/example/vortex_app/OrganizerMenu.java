44d4de8207b74d68f1fb68b328a1a4a352840c26
hlenna <hleung1@ualberta.ca>
2024-11-04 01:01:51 -0700

    Created Main menu for Organizer and, Events for Organizer

---
+++ app/src/main/java/com/example/vortex_app/OrganizerMenu.java
@@ -0,0 +1,128 @@
+package com.example.vortex_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OrganizerMenu extends AppCompatActivity {
+
+    private TextView eventNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.organizer_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        //Retrieve data for specific Event
+        String eventName = getIntent().getStringExtra("EVENT_NAME");
+        String classDay = getIntent().getStringExtra("CLASS_DAY");
+        String time = getIntent().getStringExtra("TIME");
+        String period = getIntent().getStringExtra("PERIOD");
+        String regDueDate = getIntent().getStringExtra("REG_DUE_DATE");
+        String regOpenDate = getIntent().getStringExtra("REG_OPEN_DATE");
+        double price = getIntent().getDoubleExtra("PRICE", 0.0);
+        String location = getIntent().getStringExtra("LOCATION");
+        int maxPeople = getIntent().getIntExtra("MAX_PEOPLE", 0);
+        String difficulty = getIntent().getStringExtra("DIFFICULTY");
+
+        //add other necessary data
+
+
+        // Get references to button objects on the screen
+        Button buttonOrganizerInfo = findViewById(R.id.button_info);
+        Button buttonOrganizerWaiting = findViewById(R.id.button_waiting);
+        Button buttonOrganizerQr = findViewById(R.id.button_Qrcode);
+        Button buttonOrganizerSelected = findViewById(R.id.button_selected);
+        Button buttonOrganizerMedia = findViewById(R.id.button_media);
+        Button buttonOrganizerCancel = findViewById(R.id.button_cancellation);
+        Button buttonOrganizerLocal = findViewById(R.id.button_location);
+        Button buttonOrganizerFinal = findViewById(R.id.button_final);
+
+        // Initialize the TextView
+        eventNameTextView = findViewById(R.id.text_event_name);
+        // Set the text of the TextView to the event name
+        eventNameTextView.setText(eventName);
+
+
+        // Initialize onClickListeners for the 3 buttons
+        buttonOrganizerInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+        buttonOrganizerWaiting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+        buttonOrganizerQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+        buttonOrganizerSelected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+        buttonOrganizerMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+        buttonOrganizerCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+        buttonOrganizerLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        buttonOrganizerFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
