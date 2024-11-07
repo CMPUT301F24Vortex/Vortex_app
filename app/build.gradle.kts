ff407b634e4a43f8e3b5f565e096f8fbdde6f38a
Alex E <emeny@ualberta.ca>
2024-11-06 18:13:21 -0700

    fixed deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,9 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore") // Firestore dependency without version
 
     implementation("com.google.zxing:core:3.5.1")
+
+    implementation("com.google.firebase:firebase-storage")
+
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+
 }
