166604ef7d45354e573cb28028cf5ff2475e3cd5
Alex E <emeny@ualberta.ca>
2024-11-08 09:16:36 -0700

    trying to generate javadoc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //implementation(files("/home/alex/Android/Sdk/platforms/android-35/android.jar"))
 }
